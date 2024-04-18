f01de359b99c9f64776c67555d62f4808b10f09f
Vaibhav Rameshwar Mali <malivaibhav.7620@gmail.com>
2024-04-18 09:45:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function updateSize() {
+			var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+			var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+			document.getElementById('sizeDisplay').innerHTML = "Width: " + width + ", Height: " + height;
+		}
+
+		window.addEventListener('resize', updateSize);
+		updateSize();
